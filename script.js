aa465aa2cd7790e3e5cc5507bf587d4784ab6e1c
Jordan Leonard <leonard_2010@live.com>
2026-04-08 22:24:58 -0500

    added sort and remove duplicates from array

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 class Node {
     constructor() {
-
+        this.node = node;
+        this.left = left; 
+        this.right = right;
     }
 }
 
 class Tree {
     constructor (array) {
+        array = this.cleanedArray(array);
+        this.root = this.buildTree(array);
+    }
 
+    cleanedArray(array) {
+        array = array.sort((a, b) => a - b);
+        for(let i = 0; i < array.length; i++) {
+            if(array[i] === array[i+1]){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+        return array;
+    }
+
+    buildTree(array) {
+        
+
+        
     }
 }
